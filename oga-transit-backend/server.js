20da0342f9a4be6203e8ddfb32d10b64b55ae67c
alameen16 <akintolaalameen5@gmail.com>
2026-04-30 16:31:10 +0100

    minor changes

--- oga-transit-backend/server.js
+++ oga-transit-backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://your-app.vercel.app"
+    "https://bus-portal-rho.vercel.app/"
   ],
   credentials: true,
 }));
